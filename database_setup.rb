4fb25bae4adbe031fcb8ce26f1dea6d121ba2c00
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-20 22:26:13 -0500

    Set up database and require files

--- database_setup.rb
+++ database_setup.rb
@@ -1,40 +1,14 @@
-require 'rubygems'
-require 'bundler/setup'
+# Create database for this program.
+DATABASE = SQLite3::Database.new("beer.db")
 
-require "pry"
-require "sinatra"
-require "sinatra/reloader"
-require "bcrypt"
-require "active_record"
-
-configure :development do
-  require "sqlite3"
-  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'beer.db')
+unless ActiveRecord::Base.connection.table_exists?(:beers)
+  ActiveRecord::Base.connection.create_table :users do |t|
+    t.string :name
+    t.string :description
+    t.float :abv
+    t.string :brewery
+    t.integer :ibu
+    t.integer :styleId
+  end  
 end
 
-configure :production do  
-  require "pg"
-  db = URI.parse(ENV['DATABASE_URL'])
-
-  ActiveRecord::Base.establish_connection(
-    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-end
-
-set :sessions, true
-
-# So that ActiveRecord explains the SQL it's running in the logs.
-ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
-
-# Database
-require_relative 'database_setup.rb'
-
-# Models
-
-
-# Controllers
